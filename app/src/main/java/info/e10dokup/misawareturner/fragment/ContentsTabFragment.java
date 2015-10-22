37bd0db6429da272d3c7f88bd730679401abcaf6
Yoshihiro WADA <1365196t@stu.kobe-u.ac.jp>
2015-10-22 23:53:00 +0900

    Add ContentsTabFragment and it's Layout

---
+++ app/src/main/java/info/e10dokup/misawareturner/fragment/ContentsTabFragment.java
@@ -0,0 +1,13 @@
+package info.e10dokup.misawareturner.fragment;
+
+import info.e10dokup.misawareturner.core.BaseFragment;
+
+/**
+ * Created by e10dokup on 2015/10/22
+ **/
+public class ContentsTabFragment extends BaseFragment {
+    private static final String TAG = ContentsTabFragment.class.getSimpleName();
+    private final ContentsTabFragment self = this;
+
+
+}
